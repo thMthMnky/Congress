210246e6393291fc1e2737f12bbaca7569550c03
Henry O. Zorrilla <thmthmnky@gmail.com>
2020-01-30 01:03:25 -0800

    Adding 'pipenv' support

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,5 @@
 import requests
+
 from bs4 import BeautifulSoup as soup
 # https://www.congress.gov/search?q=%7B%22subject%22%3A%22Crime+and+Law+Enforcement%22%7D&searchResultViewType=expanded&KWICView=false&pageSize=100&page=1
 my_url = "https://www.congress.gov/search?q={%22subject%22:%22Crime+and+Law+Enforcement%22}&pageSize=100&page=2&searchResultViewType=expanded&KWICView=false"
@@ -6,5 +7,8 @@ page_html = requests.get(my_url)
 page_soup = soup(page_html.content, "html.parser")
 main_container = page_soup.find("div", attrs={'id': 'main'})
 bill_headers = main_container.findAll("span", attrs={'class': 'result-heading'})
+# bill_main_link = bill_headers.findAll("a", attrs={'':''})
 if __name__ == "__main__":
-    print(bill_headers)
+    print("len(bill_headers): ", bill_headers.__len__())
+    print("bill_headers[" + str(len(bill_headers) - 1) + "]: ", bill_headers[bill_headers.__len__()-1])
+#    print("len(bill_main_link): " + len(bill_main_link))
